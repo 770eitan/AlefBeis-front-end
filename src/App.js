2e43f2bdc6b1fe70279bc9fe93788afd642575dd
Eitan Shmuel Corenblum <carl158@gmail.com>
2022-03-27 12:33:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import { Route, Routes, NavLink } from 'react-router-dom'
+import './App.css'
+import AddLetter from './pages/AddLetter/AddLetter'
 function App() {
+  const [letters, setLetters] = useState([])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Alef-Beis Letters!
+        <nav>
+          <NavLink to='/'>Letters List</NavLink>
+          <NavLink className='m-left' to='/add'>Add Letter</NavLink>
+        </nav>
       </header>
+      <main>
+        <Routes>
+          <Route path='/add' element={<AddLetter/>} />
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/AddLetter/AddLetter.jsx
@@ -0,0 +1,79 @@
+import { useState, useRef, useEffect } from "react";
+
+function AddLetter(props) {
+  const formElement = useRef();
+  const [validForm, setValidForm] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    meaning: "",
+    value: "",
+  });
+
+  useEffect(()=>{
+formElement.current.checkValidity() ? setValidForm(true) : setValidForm(false)
+  },[formData])
+  const handleChange = (evt) => {
+	  console.log(formElement)
+    setFormData({ ...formData, [evt.target.name]: evt.target.value });
+  };
+  return (
+    <>
+      <h1>Add Letter</h1>
+      <form autoComplete="off" ref={formElement}>
+        <div className="form-group mb-3">
+          <label htmlFor="name-input" className="form-label">
+            Letter's Name (required)
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name-input"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group mb-3">
+          <label htmlFor="meaning-input" className="form-label">
+            Letter's meaning (required)
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="meaning-input"
+            name="meaning"
+            value={formData.meaning}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group mb-4">
+          <label htmlFor="value-input" className="form-label">
+            Letter's value
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="value-input"
+            name="value"
+            onChange={handleChange}
+            f
+            value={formData.value}
+          />
+        </div>
+        <div className="d-grid">
+          <button
+            type="submit"
+            className="btn btn-primary btn-fluid"
+            disabled={!validForm}
+          >
+            Add Letter
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default AddLetter;
